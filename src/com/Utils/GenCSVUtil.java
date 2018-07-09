1b3c250f295c5f4c08062ad97ab9408301a9134d
chuwenjun <chuwenjun@aicaigroup.com>
2018-07-09 17:26:54 +0800

    csv工具类提交

--- src/com/Utils/GenCSVUtil.java
+++
@@ -1,7 +0,0 @@
-package com.Utils;
-
-/**
- * Created by chuwenjun on 2018/7/9 下午5:20
- */
-public class GenCSVUtil {
-}
---
+++ src/com/Utils/GenCsvFileUtil.java
@@ -0,0 +1,70 @@
+package com.Utils;
+
+
+import java.io.File;
+
+/**
+ * Created by chuwenjun on 2018/7/9 上午11:04
+ */
+public class GenCsvFileUtil {
+    /**
+     * 根据对象生成csv文件
+     */
+
+    public static void createCsvForObject(String methodName,Boolean isNormal,String objectName){
+        String suffix;
+        String objectcsvPath;
+        String testAbsolutePath;
+        if(isNormal){
+            suffix="Normal";
+        }else{
+            suffix="FuncException";
+        }
+        testAbsolutePath=System.getProperty("user.dir");
+        objectcsvPath = testAbsolutePath + "/src/test/resources/testers"+ File.separator+suffix.toLowerCase()+File.separator+methodName+File.separator+objectName+".csv";
+//        CSVUtil.createVerticalCsvForCheck(objectName.getClass(),objectcsvPath);
+
+    }
+
+    /**
+     *  生成需要校验表的csv文件
+     */
+
+    public static void createCsvForDbCheck(String methodName,Boolean isNormal,String objectName){
+        String suffix;
+        String objectcsvPath;
+        String testAbsolutePath;
+        if(isNormal){
+            suffix="Normal";
+        }else{
+            suffix="FuncException";
+        }
+        testAbsolutePath=System.getProperty("user.dir");
+        objectcsvPath = testAbsolutePath + "/src/test/resources/testers"+ File.separator+suffix.toLowerCase()+File.separator+methodName+File.separator+objectName+".csv";
+//        CSVUtil.createVerticalCsvForCheck(objectName.getClass(),objectcsvPath);
+
+    }
+
+    /**
+     * 生成需要插入表的csv文件
+     */
+
+    public static void createCsvForDbInsert(String methodName,Boolean isNormal,String objectName){
+        String suffix;
+        String objectcsvPath;
+        String testAbsolutePath;
+        if(isNormal){
+            suffix="Normal";
+        }else{
+            suffix="FuncException";
+        }
+        testAbsolutePath=System.getProperty("user.dir");
+        objectcsvPath = testAbsolutePath + "/src/test/resources/testers"+ File.separator+suffix.toLowerCase()+File.separator+methodName+File.separator+objectName+".csv";
+//        CSVUtil.createVerticalCsvForCheck(objectName.getClass(),objectcsvPath);
+
+    }
+
+    public static void main(String[] args) {
+//        createCsvForObject();
+    }
+}
